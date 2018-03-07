0a2cae33dc14c9dd1a9d9c79b754d1c691eb5e78
EvanderWang <279252080@qq.com>
2018-03-07 17:07:23 +0800

    lib test

--- Src/main.ts
+++ Src/main.ts
@@ -1,12 +1,30 @@
 import * as Rx from "../Third/RxJS_AMD/Rx";
+import { renderbase } from "../Lib/Render/renderbase";
 
 module module_main{
     export function main(){
-        const sceneresize$ = Rx.Observable.fromEvent(window, "resize");
-        sceneresize$.subscribe( () => { console.log( "w: " + window.innerWidth + "; h: " + window.innerHeight); } );
+        let canvas = <HTMLCanvasElement>document.getElementById("scene");
+        let rtx = new renderbase.VRenderContext(canvas, window.innerWidth, window.innerHeight);
 
-        const click$ = Rx.Observable.fromEvent(window, "click");
-        click$.subscribe( (e: MouseEvent) => { console.log("x: " + e.clientX + "; y: " + e.clientY); } );
+        let scene = rtx.defaultRenderTarget.createScene();
+        scene.setViewportPercent(0,0,1,1);
+        scene.setClearColor(1,0,0,1);
+
+        let dorender = () => {
+            window.requestAnimationFrame(()=>{
+                rtx.defaultRenderTarget.render();
+            })
+        }
+
+        const sceneresize$ = Rx.Observable.fromEvent(window, "resize");
+        sceneresize$.subscribe( () => { 
+            scene.dirty();
+            rtx.resize(window.innerWidth, window.innerHeight);
+
+            dorender();
+        } );
+
+        dorender();
     }
 }
 
