ff104e2ef1ea271fd4fd01d45314fe151cfcbd02
EvanderWang <279252080@qq.com>
2018-03-08 17:28:28 +0800

    base frame inited.

--- Src/main.ts
+++ Src/main.ts
@@ -9,23 +9,40 @@ module module_main{
         let scene = rtx.defaultRenderTarget.createScene();
         scene.setViewportPercent(0,0,1,1);
         scene.setClearColor(1,0,0,1);
+        scene.dirty();
 
-        let dorender = () => {
-            window.requestAnimationFrame(()=>{
-                rtx.defaultRenderTarget.render();
-            })
-        }
+        // let dorender = () => {
+        //     window.requestAnimationFrame(()=>{
+        //         rtx.defaultRenderTarget.render();
+        //     })
+        // }
+
+        let requestAnimationFrameObservable = Rx.Observable.bindCallback(window.requestAnimationFrame);
+        const raf$ = requestAnimationFrameObservable();
+        raf$.subscribe( () => {
+            rtx.defaultRenderTarget.render();
+        } , () => {
+            console.log("fisrt err");
+        } , () => {
+            console.log("fisrt fin");
+        } );
 
         const sceneresize$ = Rx.Observable.fromEvent(window, "resize");
         sceneresize$.subscribe( () => { 
+            scene.setClearColor(0,1,0,1);
             scene.dirty();
             rtx.resize(window.innerWidth, window.innerHeight);
-
-            
-            dorender();
+            //dorender();
+            raf$.subscribe( () => {
+                rtx.defaultRenderTarget.render();
+            } , () => {
+                console.log("second err");
+            } , () => {
+                console.log("second fin");
+            } );
         } );
 
-        dorender();
+        //dorender();
     }
 }
 
